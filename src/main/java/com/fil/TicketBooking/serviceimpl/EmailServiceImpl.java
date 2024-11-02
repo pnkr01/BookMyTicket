9daa3b3f3eaf9ca234d2aedc14a2c450438b9e71
lktyagi753 <lovekush75340tyagi@gmail.com>
2024-11-02 14:58:24 +0530

    Your commit message

---
+++ src/main/java/com/fil/TicketBooking/serviceimpl/EmailServiceImpl.java
@@ -0,0 +1,41 @@
+package com.fil.TicketBooking.serviceimpl;
+
+import com.fil.TicketBooking.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.io.File;
+
+public class EmailServiceImpl implements EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+    private Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
+
+    @Override
+    public void sendEmail(String to, String subject, String msg) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+      simpleMailMessage.setTo(to);
+      simpleMailMessage.setSubject(subject);
+      simpleMailMessage.setText(msg);
+        mailSender.send(simpleMailMessage);
+        logger.info("email has been sent...");
+    }
+
+    @Override
+    public void sendEmail(String[] to, String subject, String msg) {
+
+    }
+
+    @Override
+    public void sendEmailWithHtml(String to, String subject, String htmlContent) {
+
+    }
+
+    @Override
+    public void sendEmailWithFile(String to, String subject, String msg, File file) {
+
+    }
+}
--- src/main/java/com/fil/TicketBooking/serviceimpl/PaymentServiceImpl.java
+++ src/main/java/com/fil/TicketBooking/serviceimpl/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.fil.TicketBooking.serviceimpl;
+import com.fil.TicketBooking.exception.PaymentException;
 import com.fil.TicketBooking.model.Payment;
+import com.fil.TicketBooking.model.User;
 import com.fil.TicketBooking.repository.PaymentRepository;
 import com.fil.TicketBooking.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,41 +12,90 @@ import java.util.Optional;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
-
-    private final PaymentRepository paymentRepository;
-
-    @Autowired
-    public PaymentServiceImpl(PaymentRepository paymentRepository) {
-        this.paymentRepository = paymentRepository;
+    @Override
+    public Payment createOrder(User user) {
+        return null;
     }
 
     @Override
-    public Payment createPayment(Payment payment) {
-        return paymentRepository.save(payment);
+    public Payment findPaymentById(Long orderId) throws PaymentException {
+        return null;
     }
 
     @Override
-    public Payment updatePayment(Long id, Payment payment) {
-        Optional<Payment> existingPayment = paymentRepository.findById(id);
-        if (existingPayment.isPresent()) {
-            payment.setPaymentId(id);
-            return paymentRepository.save(payment);
-        }
-        return null; // or throw an exception
+    public List<Payment> usersOrderHistory(Long userId) {
+        return null;
     }
 
     @Override
-    public void deletePayment(Long id) {
-        paymentRepository.deleteById(id);
+    public Payment placedOrder(Long orderId) throws PaymentException {
+        return null;
     }
 
     @Override
-    public Payment getPaymentById(Long id) {
-        return paymentRepository.findById(id).orElse(null);
+    public Payment confirmedOrder(Long orderId) throws PaymentException {
+        return null;
     }
 
     @Override
-    public List<Payment> getAllPayments() {
-        return paymentRepository.findAll();
+    public Payment shippedOrder(Long orderId) throws PaymentException {
+        return null;
     }
+
+    @Override
+    public Payment deliveredOrder(Long orderId) throws PaymentException {
+        return null;
+    }
+
+    @Override
+    public Payment cancledOrder(Long orderId) throws PaymentException {
+        return null;
+    }
+
+    @Override
+    public List<Payment> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long orderId) throws PaymentException {
+
+    }
+
+//    private final PaymentRepository paymentRepository;
+//
+//    @Autowired
+//    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+//        this.paymentRepository = paymentRepository;
+//    }
+//
+//    @Override
+//    public Payment createPayment(Payment payment) {
+//        return paymentRepository.save(payment);
+//    }
+//
+//    @Override
+//    public Payment updatePayment(Long id, Payment payment) {
+//        Optional<Payment> existingPayment = paymentRepository.findById(id);
+//        if (existingPayment.isPresent()) {
+//            payment.setPaymentId(id);
+//            return paymentRepository.save(payment);
+//        }
+//        return null; // or throw an exception
+//    }
+//
+//    @Override
+//    public void deletePayment(Long id) {
+//        paymentRepository.deleteById(id);
+//    }
+//
+//    @Override
+//    public Payment getPaymentById(Long id) {
+//        return paymentRepository.findById(id).orElse(null);
+//    }
+//
+//    @Override
+//    public List<Payment> getAllPayments() {
+//        return paymentRepository.findAll();
+//    }
 }
